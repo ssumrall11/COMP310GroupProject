e3735717af328e3d2a83e071598ddfc3e8eadf05
Sarah Sumrall <ssumrall@luc.edu>
2024-04-21 13:12:25 -0500

    AST PROCESS Commit

--- rforth/ast.c
+++ rforth/ast.c
@@ -44,7 +44,67 @@ AST_node_t* generate_AST(map_t* worddefs,char* str,char* triggerword){
   return node;
 }
 
-void process_AST(AST_node_t* root){}
+void process_AST(AST_node_t* root){
+  LL_node_t* c = root->children->head;
+  while(c != NULL){
+    if(c->value.node == NULL){
+      token_t* token = value.token;
+     if(0==strcmp(token->str,".s")){
+          int_stack_print(stack,stdout);
+          output=1;
+        }else if(0==strcmp(token->str,"cr")){
+          int_stack_cr(stdout);
+        }else if(0==strcmp(token->str,".")){
+          int_stack_dot(stack,stdout);
+          output=1;
+        }else if(0==strcmp(token->str,"emit")){
+          int_stack_emit(stack,stdout);
+          output=1;
+        }else if(0==strcmp(token->str,"mod")){
+          int_stack_mod(stack);
+        }else if(0==strcmp(token->str,"/mod")){
+          int_stack_dividemod(stack);
+        }else if(0==strcmp(token->str,"=")){
+          int_stack_equal(stack);
+        }else if(0==strcmp(token->str,"<")){
+          int_stack_less(stack);
+        }else if(0==strcmp(token->str,">")){
+          int_stack_greater(stack);
+        }else if(0==strcmp(token->str,"invert")){
+          int_stack_invert(stack);
+        }else if(0==strcmp(token->str,"and")){
+          int_stack_and(stack);
+        }else if(0==strcmp(token->str,"or")){
+          int_stack_or(stack);
+        }else if(0==strcmp(token->str,"swap")){
+          int_stack_swap(stack);
+        }else if(0==strcmp(token->str,"dup")){
+          int_stack_dup(stack);
+        }else if(0==strcmp(token->str,"over")){
+          int_stack_over(stack);
+        }else if(0==strcmp(token->str,"drop")){
+          int_stack_drop(stack);
+        }else if(0==strcmp(token->str,"rot")){
+          int_stack_rot(stack);
+        }else if(0==strcmp(token->str,"2swap")){
+          int_stack_2swap(stack);
+        }else if(0==strcmp(token->str,"2dup")){
+          int_stack_2dup(stack);
+        }else if(0==strcmp(token->str,"2over")){
+          int_stack_2over(stack);
+        }else if(0==strcmp(token->str,"2drop")){
+          int_stack_2drop(stack);
+        }else if(0==strcmp(token->str,"2rot")){
+          int_stack_2rot(stack);
+        }else if(0==strcmp(token->str,"depth")){
+          int_stack_depth(stack);
+        }
+    }else{ // c->value.token==NULL
+       process_AST(c->value.node);
+    }
+    c = c->next; 
+  }
+}
 
 void free_AST(AST_node_t* root){
   //(added children_len field to AST_node to replace this) use root.token to determine the size of root.children?
